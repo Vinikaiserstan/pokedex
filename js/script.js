b454cbe32d3a112d0665a9cc8cbe26063a67c82e
Vinicius Neiva <viniciusneiva125@gmail.com>
2024-05-01 21:17:45 -0300

    logica de busca e carregamento de informação

--- js/script.js
+++ js/script.js
@@ -1,21 +1,48 @@
 const pokemonName = document.querySelector('.pokemon_name');
 const pokemonNumber = document.querySelector('.pokemon_number');
 const pokemonImage = document.querySelector('.pokemon_image');
+const form = document.querySelector('.form');
+const input = document.querySelector('.input_search');
+const buttonPrev = document.querySelector('.btv-prev');
+const buttonNext = document.querySelector('.btv-Next');
 
 const fetchPokemon = async (pokemon) =>{
 
-    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
-    const data = await APIResponse.json();
-    return data;
+    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.toLowerCase()}`);
+    
+    if (APIResponse.status == 200){
+        const data = await APIResponse.json();
+        return data;
+    }
+
+    
 
 }
 
 const renderPokemon = async (pokemon) => {
+    pokemonName.innerHTML = 'loading....';
+    pokemonNumber.innerHTML = '';
+
     const data = await fetchPokemon (pokemon);
 
-    pokemonName.innerHTML = data.name;
-    pokemonNumber.innerHTML = data.id;
-    pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default']
+    if (data) {
+        pokemonName.innerHTML = data.name;
+        pokemonNumber.innerHTML = data.id;
+        pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default']
+        input.value = '';
+    } else {
+        pokemonName.innerHTML = 'not found';
+        pokemonNumber.innerHTML = '';
+    }
+
+    
 }
 
-renderPokemon('600')
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    renderPokemon(input.value);
+
+})
+
+renderPokemon('1')
